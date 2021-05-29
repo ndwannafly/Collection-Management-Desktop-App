72b93b4b62d035a7f1adcad117aef6956f8c795d
Nguyen Ngoc Duc <72563171+ndwannafly@users.noreply.github.com>
2021-05-29 19:12:26 +0300

    some things were hidden

--- LAB_7/Server/src/main/java/Database/DatabaseCommunicator.java
+++ LAB_7/Server/src/main/java/Database/DatabaseCommunicator.java
@@ -5,24 +5,32 @@ import Utils.Logging;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.logging.Level;
 
 public class DatabaseCommunicator {
-/*    // on helios
-    private static final String DB_URL = "jdbc:postgresql://pg:5432/studs";
-    private static final String USER   = "s308554";
-    private static final String PASS   = "zyo564";*/
+
+    // on helios
+    // private static final String DB_URL = "jdbc:postgresql://pg:5432/studs";
+
     // on local PC
     private static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/postgres";
-    private static final String USER   = "postgres";
-    private static final String PASS   = "zyo564";
+
     private static Connection connection;
     private static PersonBase personBase;
     private static SecretBase users;
 
     public void start(){
+        Logging.log(Level.INFO, "Connecting to database ....... !");
         try {
-            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            Scanner scanner = new Scanner(System.in);
+            Logging.log(Level.INFO, "Enter your account:");
+            //System.out.println("Enter your account:");
+            String user = scanner.nextLine();
+            Logging.log(Level.INFO, "Enter your password:");
+            //System.out.println("Enter your password:");
+            String pass = scanner.nextLine();
+            connection = DriverManager.getConnection(DB_URL, user, pass);
             users = new SecretBase(connection);
             personBase = new PersonBase(connection);
         } catch (SQLException e) {
